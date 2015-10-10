1476441e1fd9a6020f9cad2763a51050a773818c
npendery <npendery@gmail.com>
2015-10-10 18:06:18 -0400

    more hound

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,8 +5,7 @@ class ConversationsController < ApplicationController
 
   def create
     if Conversation.between(params[:sender_id], params[:recipient_id]).present?
-      @conversation = Conversation.between(params[:sender_id],
-                                           params[:recipient_id]).first
+      @conversation = Conversation.between(params[:sender_id, :recipient_id]).first
     else
       @conversation = Conversation.create!(conversation_params)
     end
@@ -22,7 +21,7 @@ class ConversationsController < ApplicationController
   end
 
   private
-  
+
   def conversation_params
     params.permit(:sender_id, :recipient_id)
   end
