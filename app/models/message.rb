6dd0af978602e8bffb4dd9dd25accb2eaa7e78e4
npendery <npendery@gmail.com>
2015-10-10 18:13:36 -0400

    more hound

--- app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :conversation
-  belongs_to :user
-
-  validates :body, presence: true
-  validates :conversation_id, presence: true
-  validates :user_id, presence: true
-end
